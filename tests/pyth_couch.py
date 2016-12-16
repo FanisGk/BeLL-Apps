3a9746f9f0aca318a634672ea39d21de28149ee5
FanisGk <fanhs_95@hotmail.com>
2016-12-16 17:49:56 +0200

    create couchdb user and test login

---
+++ tests/pyth_couch.py
@@ -0,0 +1,40 @@
+import pycouchdb
+
+def create_member():
+	server = pycouchdb.Server("http://nation:oleoleole@localhost:5981/")
+	db = server.database("members")
+	db.save(
+	{
+		"kind": "Member",
+	   "roles": [
+		   "Learner"
+	   ],
+	   "bellLanguage": "English",
+	   "firstName": "q",
+	   "lastName": "q",
+	   "middleNames": "q",
+	   "login": "q",
+	   "password": "",
+	   "phone": "q",
+	   "email": "q",
+	   "language": "q",
+	   "BirthDate": "1915-12-31T22:00:00.000Z",
+	   "visits": 0,
+	   "Gender": "Male",
+	   "levels": "1",
+	   "status": "active",
+	   "community": "NATION",
+	   "region": "",
+	   "nation": "earthbell",
+	   "lastLoginDate": "2016-12-15T22:00:00.000Z",
+	   "lastEditDate": "2016-12-16T14:48:32.771Z",
+	   "credentials": {
+		   "salt": "7694f4a66316e53c8cdd9d9954bd611d",
+		   "value": "5605c1a22a715e2cb1966a1f4e244e87588c385c",
+		   "login": "q"
+	   }
+    }
+    )
+
+
+
---
+++ tests/test_login_bycouchdb.py
@@ -0,0 +1,48 @@
+import unittest
+import bell
+
+import pyth_couch
+
+from base_case import on_platforms
+from base_case import browsers
+from base_case import BaseCase
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+@on_platforms(browsers)
+class LoginTestCouchDB(BaseCase):
+	
+	
+	def test_login_bycouchdb(self):
+		pyth_couch.create_member()
+		driver = self.driver
+        
+        # login
+		bell.login_test(self.driver, "q", "q")
+        
+        # wait for the next page, and fill the configuration only if needed
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "dashboard")))
+		self.logout_test()
+		
+	def logout_test(self):
+		""" NoneType -> NoneType
+
+		Helper function testing a correct logout operation.
+		"""
+		driver = self.driver
+		# test logout
+		bell.logout(driver)
+		# ensure logout was successful
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "login")))
+		actual = driver.current_url
+		expected = "http://127.0.0.1:5981/apps/_design/bell/MyApp/index.html#login"
+		self.assertEqual(actual, expected)	
+		
+if __name__ == "__main__":
+    unittest.main()
+        
+    
